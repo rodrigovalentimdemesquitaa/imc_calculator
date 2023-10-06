29a2b4c1ad116859498a578c742058dc5a31fbe4
Rodrigo Valentim de Mesquitaa <104801608+rodrigovalentimdemesquitaa@users.noreply.github.com>
2023-10-06 20:07:56 -0300

    Projeto finalizado

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const calcular = document.getElementById('calcular');
 
-function imc () {
+function imc() {
     const nome = document.getElementById('nome').value;
     const altura = document.getElementById('altura').value;
     const peso = document.getElementById('peso').value;
@@ -12,26 +12,33 @@ function imc () {
 
         let classificacao = '';
 
-        if (valorIMC < 18.5){
+        if (valorIMC < 18.5) {
             classificacao = 'abaixo do peso.';
-        }else if (valorIMC < 25) {
-            classificacao = 'com peso ideal. Parabéns!!!';
-        }else if (valorIMC < 30){
+        } else if (valorIMC < 25) {
+            classificacao = 'com peso ideal. <br>Parabéns!!!';
+        } else if (valorIMC < 30) {
             classificacao = 'levemente acima do peso.';
-        }else if (valorIMC < 35){
+        } else if (valorIMC < 35) {
             classificacao = 'com obesidade grau I.';
-        }else if (valorIMC < 40){
+        } else if (valorIMC < 40) {
             classificacao = 'com obesidade grau II';
-        }else {
-            classificacao = 'com obesidade grau III. Cuidado!!';
+        } else {
+            classificacao = 'com obesidade grau III.<br>Cuidado!!';
         }
 
-        resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
-        
-    }else {
-        resultado.textContent = 'Preencha todos os campos!!!';
-    }
+        resultado.innerHTML = `${nome},<br>Seu IMC é ${valorIMC}.<br>Você está ${classificacao}`;
+        resultado.style.color = 'black';
+        resultado.style.fontSize = '1.3em';
+        resultado.style.textAlign = 'justify';
 
+    } else {
+        resultado.textContent = 'Preencha todos os campos!!!';
+        resultado.style.fontSize = '1.6em';
+        resultado.style.color = 'red';
+        resultado.style.textAlign = 'center';
+
+
+    }
 }
 
 calcular.addEventListener('click', imc);
